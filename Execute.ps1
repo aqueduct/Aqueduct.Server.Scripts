819410c389397a2e34d86e67ae8effcaf3c6fb88
Neil Duncan <neilduncan1234@gmail.com>
2015-02-18 14:03:45 +0000

    initial commit

---
+++ Execute.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module -Force $PSScriptRoot\Maintenance-Helpers
+
+Get-Content $PSScriptRoot\folders.txt | ForEach-Object {Remove-FilesOlderThan $_ -days 7}
+ 
---
+++ Maintenance-Helpers.psm1
@@ -0,0 +1,28 @@
+﻿function Remove-FilesOlderThan
+{
+    Param(
+        [Parameter(Mandatory=$true)]
+        [String] 
+        $folderPath,        
+        [Int] 
+        $days = 30,        
+        [Switch] 
+        $recurse
+    )
+
+    $items = Get-ChildItem $folderPath -Recurse:$recurse
+    $now = Get-Date
+    foreach($item in $items)
+    {
+        $age = ($now - $item.CreationTime).Days
+        $deleted = 0;
+        if ($age -gt $days -and $item.PsISContainer -ne $True)
+        {
+            $item.Delete()
+            $deleted += 1
+        }
+    }
+    Write-Host Deleted $deleted files from $folderPath
+}
+
+export-modulemember -function Remove-FilesOlderThan
