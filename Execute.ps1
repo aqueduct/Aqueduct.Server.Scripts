909cb205754039a831352661e553e0c88924c110
Neil Duncan <neilduncan1234@gmail.com>
2015-02-18 15:32:46 +0000

    some improvements

--- Execute.ps1
+++ Execute.ps1
@@ -1,4 +1,4 @@
 ﻿Import-Module -Force $PSScriptRoot\Maintenance-Helpers
 
-Get-Content $PSScriptRoot\folders.txt | ForEach-Object {Remove-FilesOlderThan $_ -days 7}
+Get-Content $PSScriptRoot\folders.txt | ForEach-Object {Remove-FilesOlderThan $_ -days 7 -recurse}
  
--- Maintenance-Helpers.psm1
+++ Maintenance-Helpers.psm1
@@ -10,19 +10,34 @@
         $recurse
     )
 
-    $items = Get-ChildItem $folderPath -Recurse:$recurse
+    $items = Get-ChildItem "$folderPath" -Recurse:$recurse
     $now = Get-Date
+    $deleted = 0;
+    $failed = 0;
+
+    Write-Host Found $items.Length files in $folderPath    
+    
     foreach($item in $items)
     {
         $age = ($now - $item.CreationTime).Days
-        $deleted = 0;
-        if ($age -gt $days -and $item.PsISContainer -ne $True)
+        if ($age -gt $days -and $item.PsISContainer -ne $true)
         {
-            $item.Delete()
-            $deleted += 1
+            try
+            {
+                Remove-Item $item.FullName -Force -ErrorAction Stop
+                $deleted += 1
+            }
+            catch 
+            {
+                $failed += 1;
+            }
         }
     }
-    Write-Host Deleted $deleted files from $folderPath
+    Write-Host ----> Deleted $deleted files -ForegroundColor Green
+    if ($failed -gt 0)
+    { 
+        Write-Host ----> Failed to delete $failed files -ForegroundColor Red
+    }
 }
 
 export-modulemember -function Remove-FilesOlderThan
